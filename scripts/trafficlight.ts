611287df9e0562fdbbdf2d85a1c25b60f560337e
Bruno Windels <274386+bwindels@users.noreply.github.com>
2022-09-12 17:34:42 +0200

    convert to playwright

--- scripts/trafficlight.ts
+++ scripts/trafficlight.ts
@@ -16,20 +16,20 @@ limitations under the License.
 
 /* eslint no-constant-condition: [ "error", { "checkLoops": false } ], prefer-template: 1 */
 
-import puppeteer from 'puppeteer-core';
+import {chromium} from 'playwright';
+import type {Page} from 'playwright';
 import fetch from 'node-fetch';
 import * as crypto from 'crypto';
 import * as process from 'process';
 
-async function startClient() {
-    const browser = await puppeteer.launch({
+async function startClient(elementUrl: string) {
+    const browser = await chromium.launch({
         executablePath: "/usr/bin/chromium-browser",
         headless: false,
-        // don't exit
     });
     const page = await browser.newPage();
     page.setDefaultTimeout(15000);
-    await page.goto("http://localhost:8080");
+    await page.goto(elementUrl);
     return {browser, page};
 }
 
@@ -49,20 +49,27 @@ async function registerAsClient(trafficlightUrl: string, uuid: string) {
     }
 }
 
-async function startRegisterLoop(trafficlightUrl: string) {
+async function startRegisterLoop(trafficlightUrl: string, elementUrl: string) {
     while (true) {
         const uuid = crypto.randomUUID();
         await registerAsClient(trafficlightUrl, uuid);
         const clientBaseUrl = `${trafficlightUrl}/client/${encodeURIComponent(uuid)}`;
-        const {page, browser} = await startClient();
-        await pollLoop(page, clientBaseUrl);
+        const {page, browser} = await startClient(elementUrl);
+        try {
+            await pollLoop(page, clientBaseUrl);
+        } catch (err) {
+            console.log(err);
+            console.log("------------------ stalling process");
+            await new Promise(r => {});
+        }
     }
 }
 
 
 const trafficlightUrl = process.env.TRAFFICLIGHT_URL || 'http://127.0.0.1:5000';
+const elementUrl = process.env.ELEMENT_WEB_URL || 'http://127.0.0.1:8080';
 
-startRegisterLoop(trafficlightUrl);
+startRegisterLoop(trafficlightUrl, elementUrl);
 
 type PollData = {
     action: string;
@@ -75,7 +82,7 @@ type PollData = {
  *
  * Each cycle pulls one request from the trafficlight server and acts on it.
  */
-async function pollLoop(page: puppeteer.Page, clientBaseUrl: string): Promise<void> {
+async function pollLoop(page: Page, clientBaseUrl: string): Promise<void> {
     const pollUrl = `${clientBaseUrl}/poll`;
     const respondUrl = `${clientBaseUrl}/respond`;
 
@@ -116,137 +123,102 @@ async function pollLoop(page: puppeteer.Page, clientBaseUrl: string): Promise<vo
     }
 }
 
-async function runAction(action: string, data: Record<string, any>, page: puppeteer.Page): Promise<string | undefined> {
-    async function get(selector: string, root?: Promise<puppeteer.ElementHandle | null>): Promise<puppeteer.ElementHandle | null> {
-        return (await getRoot(root)).waitForSelector(selector);
-    }
-
-    async function getRoot(root: Promise<puppeteer.ElementHandle | null> | undefined): Promise<puppeteer.ElementHandle | puppeteer.Page> {
-        if (!root) {
-            return page;
-        }
-        const r = await root;
-        if (!r) {
-            throw new Error('root evaluated to null');
-        }
-        return r;
-    }
-
-    async function contains(label: string, root?: Promise<puppeteer.ElementHandle | null>): Promise<puppeteer.ElementHandle | null> {
-        const [element] = await (await getRoot(root)).$x(`//[contains(text(), "${label}")]`);
-        return element as puppeteer.ElementHandle;
-    }
-
-    async function click(selectorPromise: Promise<puppeteer.ElementHandle | null>): Promise<void> {
-        const el = await selectorPromise;
-        if (el) {
-            await el.click();
-        }
-    }
-
-    async function type(text: string, selectorPromise: Promise<puppeteer.ElementHandle | null>, enter: boolean = false): Promise<void> {
-        const el = await selectorPromise;
-        if (el) {
-            await el.type(text);
-            if (enter) {
-                await el.press('Enter');
-            }
-        }
-    }
-
+async function runAction(action: string, data: Record<string, any>, page: Page): Promise<string | undefined> {
     async function setLocationHash(hash: string) {
         const url = new URL(await page.url());
         url.hash = hash;
-        console.log("setLocationHash", url.toString());
         await page.goto(url.toString());
     }
 
-
     switch (action) {
         case 'register':
-            console.log("going to", `${await page.url()}/#/register`);
             await setLocationHash(`#/register`);
-            await (await get('.mx_ServerPicker_change'))!.click();
-            //get('.mx_ServerPickerDialog_continue').should('be.visible');
-            await type(data['homeserver_url']['local'], get('.mx_ServerPickerDialog_otherHomeserver'));
-            await click(get('.mx_ServerPickerDialog_continue'));
+            await page.locator('.mx_ServerPicker_change').click();
+            //page.locator('.mx_ServerPickerDialog_continue').should('be.visible');
+            await page.locator('.mx_ServerPickerDialog_otherHomeserver').type(data['homeserver_url']['local']);
+            await page.locator('.mx_ServerPickerDialog_continue').click();
             // wait for the dialog to go away
-            //get('.mx_ServerPickerDialog').should('not.exist');
-            //get('#mx_RegistrationForm_username').should('be.visible');
+            await page.locator('.mx_ServerPickerDialog').waitFor({state: 'detached'});
+            await page.locator('#mx_RegistrationForm_username').waitFor({state: 'visible'});
             // Hide the server text as it contains the randomly allocated Synapse port
-            await type(data['username'], get('#mx_RegistrationForm_username'));
-            await type(data['password'], get('#mx_RegistrationForm_password'));
-            await type(data['password'], get('#mx_RegistrationForm_passwordConfirm'));
-            await click(get('.mx_Login_submit'));
-            await click(get('.mx_UseCaseSelection_skip > .mx_AccessibleButton'));
+            await page.locator('#mx_RegistrationForm_username').type(data['username']);
+            await page.locator('#mx_RegistrationForm_password').type(data['password']);
+            await page.locator('#mx_RegistrationForm_passwordConfirm').type(data['password']);
+            await page.locator('.mx_Login_submit').click();
+            await page.locator('.mx_UseCaseSelection_skip > .mx_AccessibleButton').click();
             return 'registered';
         case 'login':
             await setLocationHash(`#/login`);
-            //get('#mx_LoginForm_username', { timeout: 15000 }).should('be.visible');
-            await click(get('.mx_ServerPicker_change'));
-            await type(data['homeserver_url']['local'], get('.mx_ServerPickerDialog_otherHomeserver'));
-            await click(get('.mx_ServerPickerDialog_continue'));
+            await page.locator('#mx_LoginForm_username').waitFor({state: 'visible'});
+            await page.locator('.mx_ServerPicker_change').click();
+            await page.locator('.mx_ServerPickerDialog_otherHomeserver').type(data['homeserver_url']['local']);
+            await page.locator('.mx_ServerPickerDialog_continue').click();
             // wait for the dialog to go away
-            //get('.mx_ServerPickerDialog').should('not.exist');
-            await type(data['username'], get('#mx_LoginForm_username'));
-            await type(data['password'], get('#mx_LoginForm_password'));
-            await click(get('.mx_Login_submit'));
+            //page.locator('.mx_ServerPickerDialog').should('not.exist');
+            await page.locator('#mx_LoginForm_username').type(data['username']);
+            await page.locator('#mx_LoginForm_password').type(data['password']);
+            await page.locator('.mx_Login_submit').click();
             return 'loggedin';
         case 'start_crosssign':
-            await click(get('.mx_CompleteSecurity_actionRow > .mx_AccessibleButton'));
+            await page.locator('.mx_CompleteSecurity_actionRow > .mx_AccessibleButton').click();
             return 'started_crosssign';
         case 'accept_crosssign':
             // Can we please tag some buttons :)
             // Click 'Verify' when it comes up
-            await click(get('.mx_Toast_buttons > .mx_AccessibleButton_kind_primary'));
+            await page.locator('.mx_Toast_buttons > .mx_AccessibleButton_kind_primary').click();
             // Click to move to emoji verification
-            await click(get('.mx_VerificationPanel_QRPhase_startOption > .mx_AccessibleButton'));
+            await page.locator('.mx_VerificationPanel_QRPhase_startOption > .mx_AccessibleButton').click();
             return 'accepted_crosssign';
         case 'verify_crosssign_emoji':
-            await click(get('.mx_VerificationShowSas_buttonRow > .mx_AccessibleButton_kind_primary'));
-            await click(get('.mx_UserInfo_container > .mx_AccessibleButton'));
+            await page.locator('.mx_VerificationShowSas_buttonRow > .mx_AccessibleButton_kind_primary').click();
+            await page.locator('.mx_UserInfo_container > .mx_AccessibleButton').click();
             return 'verified_crosssign';
         case 'idle':
             await new Promise(r => setTimeout(r, 5000));
             return;
         case 'create_room':
-            await click(get('.mx_RoomListHeader_plusButton'));
-            await click(contains('New room', get('.mx_ContextualMenu')));
-            await type(data['name'], get('.mx_CreateRoomDialog_name input'));
+            await page.locator('.mx_RoomListHeader_plusButton').click();
+            await page.locator('.mx_ContextualMenu >> text=New room').click();
+            await page.locator('.mx_CreateRoomDialog_name input').type(data['name']);
             if (data['topic']) {
-                await type(data['topic'], get('.mx_CreateRoomDialog_topic input'));
+                await page.locator('.mx_CreateRoomDialog_topic input').type(data['topic']);
             }
             // do this to prevent https://github.com/vector-im/element-web/issues/22590, weirdly
-            // get('.mx_CreateRoomDialog_name input').click();
+            // page.locator('.mx_CreateRoomDialog_name input').click();
             // cy.wait(5000);
 
-            await click(get('.mx_Dialog_primary'));
-            //get('.mx_RoomHeader_nametext').should('contain', data['name']);
+            await page.locator('.mx_Dialog_primary').click();
+            //page.locator('.mx_RoomHeader_nametext').should('contain', data['name']);
             return 'room_created';
         case 'send_message':
-            await type(
-                data['message'],
-                get('.mx_SendMessageComposer div[contenteditable=true]'),
-                true // type Enter too
-            );
-            //cy.contains(data['message']).closest('mx_EventTile').should('have.class', 'mx_EventTile_receiptSent');
-            return "message_sent";
+            {
+                const composer = page.locator('.mx_SendMessageComposer div[contenteditable=true]');
+                await composer.type(data['message']);
+                await composer.press('Enter');
+                //cy.contains(data['message']).closest('mx_EventTile').should('have.class', 'mx_EventTile_receiptSent');
+                return "message_sent";
+            }
         case 'change_room_history_visibility':
-            await click(get('.mx_RightPanel_roomSummaryButton'));
-            await click(get('.mx_RoomSummaryCard_icon_settings'));
-            await click(get(`[data-testid='settings-tab-ROOM_SECURITY_TAB']`));
+            await page.locator('.mx_RightPanel_roomSummaryButton').click();
+            await page.locator('.mx_RoomSummaryCard_icon_settings').click();
+            await page.locator(`[data-testid='settings-tab-ROOM_SECURITY_TAB']`).click();
             // should be either "shared", "invited" or "joined"
-            await click(get(`label:has(#historyVis-${data['historyVisibility']}`));
-            await click(get('.mx_Dialog_cancelButton'));
-            await click(get('[data-test-id=base-card-close-button]'));
+            // TODO: has doesn't seem to work
+            await page.locator(`label`, { has: page.locator(`#historyVis-${data['historyVisibility']}`)}).click();
+            await page.locator('.mx_Dialog_cancelButton').click();
+            await page.locator('[data-test-id=base-card-close-button]').click();
             return "changed";
         case 'invite_user':
-            await click(get('.mx_RightPanel_roomSummaryButton'));
-            await click(get('.mx_RoomSummaryCard_icon_people'));
-            await click(get('.mx_MemberList_invite'));
-            await type(data['user'], get('.mx_InviteDialog_addressBar input'), true);
-            await click(get('.mx_InviteDialog_goButton'));
-            return "invited";
+            {
+                await page.locator('.mx_RightPanel_roomSummaryButton').click();
+                await page.locator('.mx_RoomSummaryCard_icon_people').click();
+                await page.locator('.mx_MemberList_invite').click();
+                const addressBar = page.locator('.mx_InviteDialog_addressBar input');
+                await addressBar.type(data['user']);
+                await addressBar.press('Enter');
+                await page.locator('.mx_InviteDialog_goButton').click();
+                return "invited";
+            }
         default:
             console.log('WARNING: unknown action ', action);
             return;
