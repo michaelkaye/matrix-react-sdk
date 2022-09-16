a578b131bc4a32d40fce0cdea3e81d0f51e199b4
Bruno Windels <274386+bwindels@users.noreply.github.com>
2022-09-16 18:20:09 +0200

    cleanup spec code for trafficlight somewhat

--- cypress/e2e/trafficlight/register.spec.ts
+++ cypress/e2e/trafficlight/register.spec.ts
@@ -25,6 +25,12 @@ type JSONValue =
     | { [x: string]: JSONValue }
     | Array<JSONValue>;
 
+describe('traffic light client', () => {
+    it('runs a trafficlight client once', () => {
+        recurse();
+    });
+});
+
 /*
  * Core loop of the trafficlight client.
  * We call it recurse() and loop via recursion rather than traditional looping
@@ -47,101 +53,90 @@ function recurse() {
         // promote response out of the callback for future use.
         const data: JSONValue = resp.body.data;
         const action: string = resp.body.action;
-        cy.log('... got ', action, data);
-        switch (action) {
-            case 'register':
-                cy.visit('/#/register');
-                cy.get('.mx_ServerPicker_change', { timeout: 15000 }).click();
-                cy.get('.mx_ServerPickerDialog_continue').should('be.visible');
-                cy.get('.mx_ServerPickerDialog_otherHomeserver').type(data['homeserver_url']['local']);
-                cy.get('.mx_ServerPickerDialog_continue').click();
-                // wait for the dialog to go away
-                cy.get('.mx_ServerPickerDialog').should('not.exist');
-                cy.get('#mx_RegistrationForm_username').should('be.visible');
-                // Hide the server text as it contains the randomly allocated Synapse port
-                cy.get('#mx_RegistrationForm_username').type(data['username']);
-                cy.get('#mx_RegistrationForm_password').type(data['password']);
-                cy.get('#mx_RegistrationForm_passwordConfirm').type(data['password']);
-                cy.get('.mx_Login_submit').click();
-                cy.get('.mx_UseCaseSelection_skip > .mx_AccessibleButton').click();
-                sendResponse('registered');
-                break;
-            case 'login':
-                cy.visit('/#/login');
-                cy.get('#mx_LoginForm_username', { timeout: 15000 }).should('be.visible');
-                cy.get('.mx_ServerPicker_change').click();
-                cy.get('.mx_ServerPickerDialog_otherHomeserver').type(data['homeserver_url']['local']);
-                cy.get('.mx_ServerPickerDialog_continue').click();
-                // wait for the dialog to go away
-                cy.get('.mx_ServerPickerDialog').should('not.exist');
-                cy.get('#mx_LoginForm_username').type(data['username']);
-                cy.get('#mx_LoginForm_password').type(data['password']);
-                cy.get('.mx_Login_submit').click();
-                sendResponse('loggedin');
-                break;
-            case 'start_crosssign':
-                cy.get('.mx_CompleteSecurity_actionRow > .mx_AccessibleButton').click();
-                sendResponse('started_crosssign');
-                break;
-            case 'accept_crosssign':
-                // Can we please tag some buttons :)
-                // Click 'Verify' when it comes up
-                cy.get('.mx_Toast_buttons > .mx_AccessibleButton_kind_primary').click();
-                // Click to move to emoji verification
-                cy.get('.mx_VerificationPanel_QRPhase_startOption > .mx_AccessibleButton').click();
-                sendResponse('accepted_crosssign');
-                break;
-            case 'verify_crosssign_emoji':
-                cy.get('.mx_VerificationShowSas_buttonRow > .mx_AccessibleButton_kind_primary').click();
-                cy.get('.mx_UserInfo_container > .mx_AccessibleButton').click();
-                sendResponse('verified_crosssign');
-                break;
-            case 'idle':
-                cy.wait(5000);
-                break;
-            case 'create_room':
-                {
-                    cy.get('.mx_RoomListHeader_plusButton').click();
-                    cy.get('.mx_ContextualMenu').contains('New room').click();
-                    cy.get('.mx_CreateRoomDialog_name input').type(data['name']);
-                    if (data['topic']) {
-                        cy.get('.mx_CreateRoomDialog_topic input').type(data['topic']);
-                    }
-                    // do this to prevent https://github.com/vector-im/element-web/issues/22590, weirdly
-                    // cy.get('.mx_CreateRoomDialog_name input').click();
-                    // cy.wait(5000);
-
-                    cy.get('.mx_Dialog_primary').click();
-                    cy.get('.mx_RoomHeader_nametext').should('contain', data['name']);
-                    cy.request('POST', respondUrl, { response: 'room_created' }).then((response) => {
-                        expect(response.status).to.eq(200);
-                    });
-                    break;
-                }
-            case 'send_message':
-                {
-                    const composer = cy.get('.mx_SendMessageComposer div[contenteditable=true]');
-                    composer.type(data['message']);
-                    composer.type("{enter}");
-                    //cy.contains(data['message']).closest('mx_EventTile').should('have.class', 'mx_EventTile_receiptSent');
-                    cy.request('POST', respondUrl, { response: 'message_sent' }).then((response) => {
-                        expect(response.status).to.eq(200);
-                    });
-                    break;
-                }
-            case 'exit':
-                cy.log('Client asked to exit, test complete or server teardown');
-                return;
-            default:
-                cy.log('WARNING: unknown action ', action);
-                return;
+        cy.log('running action', action, JSON.stringify(data));
+        const result = runAction(action, data);
+        if (result) {
+            sendResponse(result);
         }
-        recurse();
+        if (action !== 'exit') {
+            recurse();
+        }
     });
 }
 
-describe('traffic light client', () => {
-    it('runs a trafficlight client once', () => {
-        recurse();
-    });
-});
+function runAction(action: string, data: JSONValue): string | undefined {
+    switch (action) {
+        case 'register':
+            cy.visit('/#/register');
+            cy.get('.mx_ServerPicker_change', { timeout: 15000 }).click();
+            cy.get('.mx_ServerPickerDialog_continue').should('be.visible');
+            cy.get('.mx_ServerPickerDialog_otherHomeserver').type(data['homeserver_url']['local']);
+            cy.get('.mx_ServerPickerDialog_continue').click();
+            // wait for the dialog to go away
+            cy.get('.mx_ServerPickerDialog').should('not.exist');
+            cy.get('#mx_RegistrationForm_username').should('be.visible');
+            // Hide the server text as it contains the randomly allocated Synapse port
+            cy.get('#mx_RegistrationForm_username').type(data['username']);
+            cy.get('#mx_RegistrationForm_password').type(data['password']);
+            cy.get('#mx_RegistrationForm_passwordConfirm').type(data['password']);
+            cy.get('.mx_Login_submit').click();
+            cy.get('.mx_UseCaseSelection_skip > .mx_AccessibleButton').click();
+            return 'registered';
+        case 'login':
+            cy.visit('/#/login');
+            cy.get('#mx_LoginForm_username', { timeout: 15000 }).should('be.visible');
+            cy.get('.mx_ServerPicker_change').click();
+            cy.get('.mx_ServerPickerDialog_otherHomeserver').type(data['homeserver_url']['local']);
+            cy.get('.mx_ServerPickerDialog_continue').click();
+            // wait for the dialog to go away
+            cy.get('.mx_ServerPickerDialog').should('not.exist');
+            cy.get('#mx_LoginForm_username').type(data['username']);
+            cy.get('#mx_LoginForm_password').type(data['password']);
+            cy.get('.mx_Login_submit').click();
+            return 'loggedin';
+        case 'start_crosssign':
+            cy.get('.mx_CompleteSecurity_actionRow > .mx_AccessibleButton').click();
+            return 'started_crosssign';
+        case 'accept_crosssign':
+            // Can we please tag some buttons :)
+            // Click 'Verify' when it comes up
+            cy.get('.mx_Toast_buttons > .mx_AccessibleButton_kind_primary').click();
+            // Click to move to emoji verification
+            cy.get('.mx_VerificationPanel_QRPhase_startOption > .mx_AccessibleButton').click();
+            return 'accepted_crosssign';
+        case 'verify_crosssign_emoji':
+            cy.get('.mx_VerificationShowSas_buttonRow > .mx_AccessibleButton_kind_primary').click();
+            cy.get('.mx_UserInfo_container > .mx_AccessibleButton').click();
+            return 'verified_crosssign';
+        case 'idle':
+            cy.wait(5000);
+            break;
+        case 'create_room':
+            cy.get('.mx_RoomListHeader_plusButton').click();
+            cy.get('.mx_ContextualMenu').contains('New room').click();
+            cy.get('.mx_CreateRoomDialog_name input').type(data['name']);
+            if (data['topic']) {
+                cy.get('.mx_CreateRoomDialog_topic input').type(data['topic']);
+            }
+            // do this to prevent https://github.com/vector-im/element-web/issues/22590, weirdly
+            // cy.get('.mx_CreateRoomDialog_name input').click();
+            // cy.wait(5000);
+
+            cy.get('.mx_Dialog_primary').click();
+            cy.get('.mx_RoomHeader_nametext').should('contain', data['name']);
+            return "room_created";
+        case 'send_message':
+            cy.get('.mx_SendMessageComposer div[contenteditable=true]')
+                .click()
+                .type(data['message'])
+                .type("{enter}");
+            //cy.contains(data['message']).closest('mx_EventTile').should('have.class', 'mx_EventTile_receiptSent');
+            return "message_sent";
+        case 'exit':
+            cy.log('Client asked to exit, test complete or server teardown');
+            return;
+        default:
+            cy.log('WARNING: unknown action ', action);
+            return;
+    }
+}
